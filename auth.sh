7addd6f18130de174be0fc3f88a4648c4f299450
Shezad Khan <shaz@revl.world>
2017-01-10 13:20:23 +0000

    Add encrypted cache to speed up script

--- auth.sh
+++ auth.sh
@@ -3,7 +3,15 @@
 if [ "$1" != "{{ auth_name }}" ]; then
 	exit 0
 fi
-
+secret={{ secret }}
+tmp_file="/tmp/tmp_auth_keys"
+last_modified_time=$(stat -c %Y $tmp_file 2> /dev/null || echo 0)
+current_time=$(date +%s)
+time_since_last_modified=$((current_time - last_modified_time))
+if (( time_since_last_modified < 300 )); then
+        cat $tmp_file | openssl des3 -d -salt -pass pass:$secret
+        exit 0
+fi
 instance=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | awk -F\" '/region/ {print $4}')
 groups=$(aws ec2 describe-instances --region "$region" --instance-id "$instance" --query "Reservations[*].Instances[*].Tags[?Key=='CanSSH'].Value" --output text)
@@ -19,4 +27,4 @@ for group in $groups; do
 			echo "$key"
 		done
 	done
-done | sort | uniq
+done | sort | uniq | tee >(openssl des3 -salt -out $tmp_file -pass pass:$secret)
